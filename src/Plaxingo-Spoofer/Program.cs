e29366b75f37bc7688e7a2e95ae31ef4d5acbcaa
BulletBoomer <113671873+BulletBoomer@users.noreply.github.com>
2022-09-25 23:05:33 +0300

    Add files via upload

---
+++ src/Plaxingo-Spoofer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Plaxingo_Spoofer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Plaxingo-Spoofer... Pls Use Arguments to Spoof your PC");
+            Thread.Sleep(1500);
+            Console.Clear();
+            if (args[0].StartsWith("-spoof_disk"))
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Spoofing Disks...");
+                Spoofer disk_spoof = new Spoofer();
+                disk_spoof.SpoofDisks();
+                Console.WriteLine("Spoofing Disks is Complete!!!");
+                Thread.Sleep(3500);
+                Environment.Exit(122);
+            }
+            if (args[1].StartsWith("-spoof_guid"))
+            {
+                Thread.Sleep(1500);
+                Console.WriteLine("Spoofing GUID...");
+                Spoofer spoof_guid = new Spoofer();
+                spoof_guid.SpoofGUIDs();
+                Console.WriteLine("Spoofing GUID is Complete!!!");
+                Thread.Sleep(3500);
+                Environment.Exit(143);
+            }
+            if (args[2].StartsWith("-spoof_macaddress"))
+            {
+                Thread.Sleep(15000);
+                Console.WriteLine("Spoofing MAC Address");
+                Spoofer mac_spoof = new Spoofer();
+                mac_spoof.SpoofMAC();
+                Console.WriteLine("Spoofing MAC is Complete!!!");
+                Thread.Sleep(1400);
+                Environment.Exit(1224);
+            }
+        }
+    }
+}
---
+++ src/Plaxingo-Spoofer/Spoofer_Plax.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plaxingo_Spoofer
+{
+    public class Spoofer
+    {
+        public string RandomMac()
+        {
+            string chars = "ABCDEF0123456789";
+            string windows = "26AE";
+            string result = "";
+            Random random = new Random();
+
+            result += chars[random.Next(chars.Length)];
+            result += windows[random.Next(windows.Length)];
+
+            for (int i = 0; i < 5; i++)
+            {
+                result += "-";
+                result += chars[random.Next(chars.Length)];
+                result += chars[random.Next(chars.Length)];
+
+            }
+
+            return result;
+        }
+        public void SpoofGUIDs()
+        {
+            RegistryKey HardwareGUID = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\IDConfigDB\\Hardware Profiles\\0001", true);
+            HardwareGUID.SetValue("HwProfileGuid", $"{{{Guid.NewGuid()}}}");
+
+            RegistryKey MachineGUID = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Cryptography", true);
+            MachineGUID.SetValue("MachineGuid", Guid.NewGuid().ToString());
+
+            RegistryKey MachineId = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\SQMClient", true);
+            MachineId.SetValue("MachineId", $"{{{Guid.NewGuid()}}}");
+
+            RegistryKey SystemInfo = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\SystemInformation", true);
+
+            Random rnd = new Random();
+            int day = rnd.Next(1, 31);
+            string dayStr = "";
+            if (day < 10) dayStr = $"0{day}";
+            else dayStr = day.ToString();
+
+            int month = rnd.Next(1, 13);
+            string monthStr = "";
+            if (month < 10) monthStr = $"0{month}";
+            else monthStr = month.ToString();
+
+            SystemInfo.SetValue("BIOSReleaseDate", $"{dayStr}/{monthStr}/{rnd.Next(2000, 2023)}");
+            SystemInfo.SetValue("BIOSVersion", RandomId(10));
+            SystemInfo.SetValue("ComputerHardwareId", $"{{{Guid.NewGuid()}}}");
+            SystemInfo.SetValue("ComputerHardwareIds", $"{{{Guid.NewGuid()}}}\n{{{Guid.NewGuid()}}}\n{{{Guid.NewGuid()}}}\n");
+            SystemInfo.SetValue("SystemManufacturer", RandomId(15));
+            SystemInfo.SetValue("SystemProductName", RandomId(6));
+
+            RegistryKey Update = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate", true);
+            Update.SetValue("SusClientId", Guid.NewGuid().ToString());
+            Update.SetValue("SusClientIdValidation", Encoding.UTF8.GetBytes(RandomId(25)));
+        }
+
+        public bool SpoofMAC()
+        {
+            bool err = false;
+
+            RegistryKey NetworkAdapters = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}");
+            foreach (string adapter in NetworkAdapters.GetSubKeyNames())
+            {
+                if (adapter != "Properties")
+                {
+                    try
+                    {
+                        RegistryKey NetworkAdapter = Registry.LocalMachine.OpenSubKey($"SYSTEM\\CurrentControlSet\\Control\\Class\\{{4d36e972-e325-11ce-bfc1-08002be10318}}\\{adapter}", true);
+                        if (NetworkAdapter.GetValue("BusType") != null)
+                        {
+                            NetworkAdapter.SetValue("NetworkAddress", RandomMac());
+                            string adapterId = NetworkAdapter.GetValue("NetCfgInstanceId").ToString();
+
+                        }
+                    }
+                    catch (System.Security.SecurityException ex)
+                    {
+                        Console.WriteLine("\n[X] Start the spoofer in admin mode to spoof your MAC address!");
+                        err = true;
+                        break;
+                    }
+                }
+            }
+
+            return err;
+        }
+        public void SpoofDisks()
+        {
+            RegistryKey ScsiPorts = Registry.LocalMachine.OpenSubKey("HARDWARE\\DEVICEMAP\\Scsi");
+            foreach (string port in ScsiPorts.GetSubKeyNames())
+            {
+                RegistryKey ScsiBuses = Registry.LocalMachine.OpenSubKey($"HARDWARE\\DEVICEMAP\\Scsi\\{port}");
+                foreach (string bus in ScsiBuses.GetSubKeyNames())
+                {
+                    RegistryKey ScsuiBus = Registry.LocalMachine.OpenSubKey($"HARDWARE\\DEVICEMAP\\Scsi\\{port}\\{bus}\\Target Id 0\\Logical Unit Id 0", true);
+                    if (ScsuiBus != null)
+                    {
+                        if (ScsuiBus.GetValue("DeviceType").ToString() == "DiskPeripheral")
+                        {
+                            string identifier = RandomId(14);
+                            string serialNumber = RandomId(14);
+
+                            ScsuiBus.SetValue("DeviceIdentifierPage", Encoding.UTF8.GetBytes(serialNumber));
+                            ScsuiBus.SetValue("Identifier", identifier);
+                            ScsuiBus.SetValue("InquiryData", Encoding.UTF8.GetBytes(identifier));
+                            ScsuiBus.SetValue("SerialNumber", serialNumber);
+                        }
+                    }
+                }
+            }
+        }
+        public string RandomId(int length)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string result = "";
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                result += chars[random.Next(chars.Length)];
+            }
+
+            return result;
+        }
+
+    }
+}
